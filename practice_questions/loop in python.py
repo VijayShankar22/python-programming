24784966488f4b411a69546ad9599c01da4df594
Vijay Shankar <vijayshankar1035@gmail.com>
2024-01-31 23:48:46 +0530

    loop in python

---
+++ practice_questions/loop in python.py
@@ -0,0 +1,28 @@
+# Question: Print numbers from 0 to 10 using a for loop.
+
+for i in range(0, 11, 1):
+    print(i)
+
+# Question: Print multiples of 3 from 3 to 30 using a for loop.
+
+for i in range(3, 31, 3):
+    print(i)
+
+# Question: Print numbers from 10 to 2 in reverse order using a for loop.
+
+for i in range(10, 2, -1):
+    print(i)
+
+# Question: Print numbers from 1 to 10 using a while loop.
+
+i = 0
+while i <= 9:
+    i += 1
+    print(i)
+
+# Question: Print numbers from 4 to 30 using a while loop.
+
+i = 3
+while i <= 30:
+    i += 1
+    print(i)
