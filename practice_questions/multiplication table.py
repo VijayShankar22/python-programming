bcb632eeaa305c0fd7eefc738a4be5534356ae1b
Vijay Shankar <vijayshankar1035@gmail.com>
2024-04-27 17:03:36 +0530

    multiplication table.py

--- practice_questions/multiplication table.py
+++ practice_questions/multiplication table.py
@@ -1,8 +1,10 @@
-n=int(input("enter the number"))
-list=[]
-i=1
-while i<=10:
-    result=n*i
-    list.append(result)
-    i+=1
-print("multiplicative table on",n," is:",list)
+# write a python code to print the multiplication table of a given number when the number is entered by the user.
+
+n = int(input("Enter a number: "))
+i = 1
+print()
+print(f"Multiplication table of {n}") 
+print()
+while i <= 10:
+    print(f"{n} x {i} = {n*i}")
+    i += 1
